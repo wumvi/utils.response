bdabc8de7e3570080643a713e345f5dd46c745b8
VladKopalov <vladkop@mail.ru>
2020-06-18 23:44:38 +0300

    add binary response

--- phpunit/tests/ResponseTest.php
+++ phpunit/tests/ResponseTest.php
@@ -5,18 +5,56 @@ use Wumvi\Utils\Response;
 
 class ResponseTest extends TestCase
 {
-    public function testGetCryptSalt(): void
+    public function testSimple(): void
     {
         $this->assertEquals(
-            Response::jsonSuccess([1]),
             ['Content-Type: application/json', '{"status":"ok","data":[1]}'],
+            Response::jsonSuccess([1]),
             'Json Success'
         );
 
         $this->assertEquals(
-            Response::jsonError("test"),
             ['Content-Type: application/json', '{"status":"error","msg":"test"}'],
+            Response::jsonError("test"),
             'Json Error'
+        );
+
+        $act = Response::binarySuccess([1]);
+        $act[1] = base64_encode($act[1]);
+        $this->assertEquals(
+            ['Content-Type: application/octet-stream', 'AAAAAhQCEQZzdGF0dXMRAm9rEQRkYXRhFAEGAAYB'],
+            $act,
+            'Binary Success'
+        );
+
+        $act = Response::binaryError('error');
+        $act[1] = base64_encode($act[1]);
+        $this->assertEquals(
+            ['Content-Type: application/octet-stream', 'AAAAAhQCEQZzdGF0dXMRBWVycm9yEQNtc2cOAQ=='],
+            $act,
+            'Binary Error'
+        );
+    }
+
+    public function testSign(): void
+    {
+        $response = new Response('m5', '123');
+        $json = $response->successSign([1], false);
+        $this->assertEquals(
+            ['Content-Type: application/json', 'm516d1a2b7c86f137adf1d393ee596addc{"status":"ok","data":[1]}'],
+            $json,
+            'Json Success'
+        );
+
+        $act = $response->successSign([1], true);
+        $act[1] = base64_encode($act[1]);
+        $this->assertEquals(
+            [
+                'Content-Type: application/octet-stream',
+                'bTVhZDM5ZTNjNmQyZGM2ODNmOGZlMDU2M2RkZGU3ZmUyNwAAAAIUAhEGc3RhdHVzEQJvaxEEZGF0YRQBBgAGAQ=='
+            ],
+            $act,
+            'Binary Success'
         );
     }
 }
--- src/Response.php
+++ src/Response.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace Wumvi\Utils;
 
+use Wumvi\Utils\Sign;
+
 /**
  *
  *
@@ -10,12 +12,49 @@ namespace Wumvi\Utils;
  */
 class Response
 {
+    public const HEADER = 0;
+    public const DATA = 1;
     public const FIELD_STATUS = 'status';
     public const FIELD_MSG = 'msg';
     public const FIELD_DATA = 'data';
+    public const FIELD_SIGN = 'sign';
     public const STATUS_OK = 'ok';
     public const STATUS_ERROR = 'error';
     public const CONTENT_TYPE_JSON = 'Content-Type: application/json';
+    public const CONTENT_TYPE_BINARY = 'Content-Type: application/octet-stream';
+
+    private string $algo;
+    private string $salt;
+
+    public function __construct(string $algo, string $salt)
+    {
+        $this->algo = $algo;
+        $this->salt = $salt;
+    }
+
+    public function successSign(array $data, bool $isBinary = false): array
+    {
+        $data = $isBinary ? self::binarySuccess($data) : self::jsonSuccess($data);
+        $data[self::DATA] = Sign::getSignWithData($data[self::DATA], $this->salt, $this->algo);
+
+        return $data;
+    }
+
+    public static function binarySuccess(array $data): array
+    {
+        return [
+            self::CONTENT_TYPE_BINARY,
+            igbinary_serialize([self::FIELD_STATUS => self::STATUS_OK, self::FIELD_DATA => $data,]),
+        ];
+    }
+
+    public static function binaryError(string $msg): array
+    {
+        return [
+            self::CONTENT_TYPE_BINARY,
+            igbinary_serialize([self::FIELD_STATUS => self::STATUS_ERROR, self::FIELD_MSG => $msg,]),
+        ];
+    }
 
     /**
      * @param array $data
@@ -43,7 +82,7 @@ class Response
      */
     public static function flush(array $data): void
     {
-        header($data[0]);
-        echo $data[1];
+        header($data[self::HEADER]);
+        echo $data[self::DATA];
     }
 }
